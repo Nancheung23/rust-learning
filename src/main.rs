0ecc521116a9b6f0b8867f90d5ae14692e0b8db0
tyr <gdog52723@gmail.com>
2026-02-15 10:27:25 +0200

    2/15 hello world/type/function/println...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+// greeting
+fn greet_world() {
+    let chinese = "世界，你好！";
+    let english = "world, hello!";
+    let finnish = "maailmaa, terve!";
+    let regions = [chinese, english, finnish];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
+// sum
+fn add(i:i32,j:i32) -> i32 {
+    return i + j;
+}
+
 fn main() {
-    println!("Hello, world!");
+    greet_world();
+    // const a, b
+    let a = 10;
+    let b: i32 = 20;
+    // mutable c
+    let mut c = 30i32;
+    // _ for better reading xp
+    let d = 30_i32;
+    // use function return a parameter
+    let e = add(add(a, b), add(c, d));
+    println!("(a + b) + (c + d) = {}", e);
 }
