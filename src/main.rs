83fbc8ffe07efec18e6e72742445bf90bb815b4a
tyr <gdog52723@gmail.com>
2026-02-19 09:03:59 +0200

    2/19 type, range, num 0.4.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use num::complex::Complex;
+
 // a strct for number 
 struct Struct {
     e: i32
@@ -96,6 +98,47 @@ fn _define_y() -> String {
     let y = String::from("hello");
     y
 }
+// numeric type: i8->i128, isize. u8->u128, usize. f32/ f64. default in rust: i32
+// string type: &str ; char type: char for one unicode character such as 'A'
+// boolean: true/ false
+// unit type: (), same as the only value
+fn guess_type() {
+    let msg:&str = "Not a number!";
+    let guess:i32 = "42".parse().expect(msg);
+    println!("the number is: {}", guess);
+    // float
+    let mut x = 2.0;
+    let y: f32 = 3.1;
+    println!("x + y = {}", x + y);
+    // NAN
+    x = (-42.0_f32).sqrt();
+    if x.is_nan() {
+        println!("sqrt result: {}", x);
+    }
+}
+// Range
+fn range_usage() {
+    let numbers = [
+        1, 2
+    ];
+    for i in 1..=3 {
+        for num in numbers.iter() {
+            println!("{} : {} ", i,num);
+        }
+    }
+    for i in 'a'..='c' {
+        print!("{} ",i);
+    }
+    println!();
+}
+// num 0.4.0 dependency
+fn num_usage() {
+    // to represent: k + ji
+    let a = Complex {re: 2.1, im: -1.2};
+    let b = Complex::new(11.1, 22.2);
+    let result = a + b;
+    println!("{} + {}i", result.re, result.im)
+}
 
 // main function
 fn main() {
@@ -105,4 +148,7 @@ fn main() {
     adv_variable();
     overwrite_variable();
     space_count();
+    guess_type();
+    range_usage();
+    num_usage();
 }
